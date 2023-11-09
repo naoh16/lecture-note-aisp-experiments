c1a43a14497bcd9e7da4ef877ae2b9efd8d130f6
Sunao Hara <hara@okayama-u.ac.jp>
2023-11-09 19:33:50 +0900

    Bump up version

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2023, Sunao Hara, Okayma University'
 #author = 'Sunao Hara'
 
 # The full version, including alpha/beta/rc tags
-release = '23.1.0'
+release = '23.2.0'
 
 
 # -- General configuration ---------------------------------------------------
